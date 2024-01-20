140adf42620d2642e545211212420b906a70a5fc
liutao <liutaohangzhou@163.com>
2024-01-20 18:07:50 +0800

    feat: 🎸 打印日志

--- log.js
+++ log.js
@@ -2,3 +2,6 @@ const os = require('os');
 const homedir = os.homedir();
 
 console.log(homedir);
+// node --inspect-brk log.js
+
+// chrome://inspect/#devices
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 
 @Controller('posts')
 export class PostsController {
+  private logger = new Logger();
   @Get('test1')
   echoTest(): string {
+    this.logger.debug('aaa', Controller.name);
+    this.logger.error('bbb', Controller.name);
+    this.logger.log('ccc', Controller.name);
+    this.logger.verbose('ddd', Controller.name);
+    this.logger.warn('eee', Controller.name);
     return 'test posts';
   }
 }
